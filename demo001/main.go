3851ac0337ce4f096f8d6f55f1a0cec930492ead
chenfeizhou <1114526565@qq.com>
2022-05-24 16:38:45 +0800

    fix: step1 抓取网页内容

--- demo001/main.go
+++ demo001/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+const URL = "https://learnku.com/go"
+
+var wg sync.WaitGroup
 
 func main() {
-	fmt.Println("hello world")
+
+	wg.Add(1)
+
+	go fetchArticles()
+
+	wg.Wait()
+
+	fmt.Println("main over")
+}
+
+func fetchArticles() {
+
+	defer wg.Done()
+
+	req, _ := http.NewRequest("GET", URL, nil)
+
+	resp, _ := http.DefaultClient.Do(req)
+
+	// 读取网页内容
+	content, _ := ioutil.ReadAll(resp.Body)
+
+	// 二进制文件转成网页内容
+	respBody := string(content)
+
+	fmt.Println(respBody)
 }
